8f58e96f3076a168c59cd1436f63518e4529c79b
Berk Sevgi <berksevgi95@hotmail.com>
2020-12-12 03:05:56 +0300

    Assignee object created

---
+++ models/Assignee.go
@@ -0,0 +1,11 @@
+package models
+
+type Assignee struct {
+	ID 				string `json:"_id" bson:"_id"`
+	Name			string `json:"name" bson:"name"`
+	Department  	string `json:"department" bson:"department"`
+}
+
+func AssigneeTableName() string {
+	return "Assignee"
+}
